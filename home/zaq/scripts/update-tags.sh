be980b089607c86fa8d47ccd008f5cd02e968480
Zaq? Wiedmann <zaqthefreshman@gmail.com>
2015-01-11 14:16:56 -0800

    pipe stderr to /dev/null to suppress warnings

--- home/zaq/scripts/update-tags.sh
+++ home/zaq/scripts/update-tags.sh
@@ -2,11 +2,9 @@
 
 if [[ ${2: -4} != ".swp" && ${2: -5} != ".swpx" && $2 != "tags" ]]; then
         if [[ "${1##$projects/go}" != "$1" ]]; then
-                echo "gotags updated"
-                gotags -R=true -f $projects/go/tags $projects/go
+                gotags -R=true -f $projects/go/tags $projects/go 2> /dev/null &
         else    
-                echo "ctags updated"
-                ctags -R --exclude=$projects/go -f $projects/tags $projects/
+                ctags -R --exclude=$projects/go -f $projects/tags $projects/ 2> /dev/null &
         fi      
 else
         echo "file ignored"
