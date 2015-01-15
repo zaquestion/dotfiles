d032d0d52a2ad5f609a94fc929253d948676a073
Zaq? Wiedmann <zaqthefreshman@gmail.com>
2015-01-14 17:22:58 -0800

    Update update-tags.sh

--- home/zaq/scripts/update-tags.sh
+++ home/zaq/scripts/update-tags.sh
@@ -2,9 +2,9 @@
 
 if [[ ${2: -4} != ".swp" && ${2: -5} != ".swpx" && $2 != "tags" ]]; then
         if [[ "${1##$projects/go}" != "$1" ]]; then
-                gotags -R=true -f $projects/go/tags $projects/go 2> /dev/null &
+                gotags -R=true -f $projects/go/tags $projects/go &> /dev/null &
         else    
-                ctags -R --exclude=$projects/go -f $projects/tags $projects/ 2> /dev/null &
+                ctags -R --exclude=$projects/go -f $projects/tags $projects/ &> /dev/null &
         fi      
 else
         echo "file ignored"
