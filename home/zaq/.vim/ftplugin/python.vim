457329f65017f1a22af6a4a3d87398aff5ca7426
Zaq? Wiedmann <zaquestion@gmail.com>
2016-11-30 15:04:22 -0800

    Remove urxvt, replaced by st (suckless)

--- home/zaq/.vim/ftplugin/python.vim
+++
@@ -1,5 +0,0 @@
-setlocal tabstop=4
-setlocal shiftwidth=4
-setlocal expandtab
-setlocal autoindent
-setlocal smarttab
--- home/zaq/scripts/update-tags.sh
+++ home/zaq/scripts/update-tags.sh
@@ -2,10 +2,9 @@
 
 if [[ ${2: -4} != ".swp" && ${2: -5} != ".swpx" && $2 != "tags" ]]; then
         if [[ "${1##$projects/go}" != "$1" ]]; then
-                gotags -R=true -f $projects/go/tags $projects/go &> /dev/null &
-        else    
+        else
                 ctags -R --exclude=$projects/go -f $projects/tags $projects/ &> /dev/null &
-        fi      
+        fi
 else
         echo "file ignored"
 fi
